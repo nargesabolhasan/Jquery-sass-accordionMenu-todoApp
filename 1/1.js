a34e0778cddda80f1328376014363b5930937fdb
narges Hajabolhasan <narges.abolhasan@gmail.com>
2022-01-30 14:20:55 +0330

    radios added

--- 1/1.js
+++ 1/1.js
@@ -1,13 +1,11 @@
-let counterT=document.getElementById("column1").innerText;
+let counterT = document.getElementById("column1").innerText;
 console.log(counterT)
 
-$(document).ready(function(){
-    $("#success").click(function(){
-        $('#myTable tr:last').after(`<tr>
-        <th scope="row">${counterT++}</th>
-        <td><input type="text"></td>
-        <td><input type="text"></td>
-        <td>@mdo</td>
-    </tr>`); 
+$(document).ready(function () {
+    $("#success").click(function () {
+        $('#myTable tr:last').after(``);
     })
+
+
+
 });
