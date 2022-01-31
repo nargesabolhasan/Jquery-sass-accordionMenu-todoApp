299ac5ea7ed1270332a12628c25fa6fd7ff3d664
narges Hajabolhasan <narges.abolhasan@gmail.com>
2022-01-31 21:08:47 +0330

    folders name changed

--- 2/assets/2.js
+++ 2/assets/2.js
@@ -2,10 +2,8 @@ $(document).ready( function() {
 	$("#accordion_wrapper ul li a").click( function() {
 		$("#accordion_wrapper ul li a").removeClass("active");
 		$(this).addClass("active");
-		
+        
 		$("#accordion_wrapper ul li div").slideUp();
-		
 		$(this).next("div").slideDown();
-		
 	});
 });
