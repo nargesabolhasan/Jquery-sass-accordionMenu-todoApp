4685a354f5badee77d0f0f57783f70ca2d28d7cc
narges Hajabolhasan <narges.abolhasan@gmail.com>
2022-01-31 18:01:11 +0330

    sass styles changed

--- 1/1.js
+++ 1/1.js
@@ -2,36 +2,21 @@ let counterT = document.getElementById("column1").innerText;
 
 
 $(document).ready(function () {
+    //.........add Row...............
     $("#success").click(function () {
-        $('#tbody tr:first').before(`<tr>
-        <th scope="row" id="column1">
-            <button type="button" class=" btn btn-danger c-Delete" >Delete</button>
-            <button type="button" class=" btn btn-primary c-clone" >clone</button>
-        </th>
-        <td><input type="text"></td>
-        <td><input type="text"></td>
-        <td >
-            <input class="form-check-input" type="radio" name="flexRadioDefault"
-                id="New">
-            <label class="form-check-label" for="flexRadioDefault1" style="margin-right:30px" >
-                New
-            </label>
-            <input class="form-check-input" type="radio" name="flexRadioDefault"
-                id="progress">
-            <label class="form-check-label" for="flexRadioDefault1" style="margin-right:30px" >
-               In progress
-            </label>
-            <input class="form-check-input" type="radio" name="flexRadioDefault"
-                id="Confirmed">
-            <label class="form-check-label " for="flexRadioDefault1">
-                Confirmed
-            </label>
-        </td>
-    </tr>`);
+        $('#tbody tr:first').before(` `);
     })
-
+    //.................delete Row...................
     $("#myTable").on('click','.c-Delete',function(){
         $(this).parent().parent().remove();
     });
-     
+    //.................clone Row....................
+  
+    $("#myTable").on('click','.c-clone',function(){
+        const thisRow = $( this ).closest( 'tr' )[0];
+        $( thisRow ).clone().insertAfter( thisRow ).val( "" );
+        
+     });
+    //...........................................
+
 });
